68b5b66497175c9ed83bfa964113a240ef6847e5
Summer <wpsumsun@126.com>
2017-04-28 17:51:34 +0800

    what

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      newTodo:'test',
+      todoList:[
+        {id:1,title:'第一个待办'}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
