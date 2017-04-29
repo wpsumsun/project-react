cd130d49e2d7ec5d25b70303d6aa7d513827a90f
summer <wpsumsun@126.com>
2017-04-30 00:43:02 +0800

    新增todo

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 
 export default class TodoInput extends Component {
     render(){
-        return <input type="text" defaultValue={this.props.content} onKeyPress={this.submit.bind(this)}/>
+        return <input type="text" defaultValue={this.props.content}
+        onKeyPress={this.submit.bind(this)}/>
     }
     submit(e){
         if(e.key==='Enter'){
-            this.props.onSubmit.call()
+            this.props.onSubmit(e)
         }
     }
 }
