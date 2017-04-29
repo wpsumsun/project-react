584cf084db5c729704667bc5fdb24d544457874d
summer <wpsumsun@126.com>
2017-04-30 01:49:37 +0800

    删除todo

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ class App extends Component {
     let todos=this.state.todoList.map((item,index)=>{
       return (
         <li key={index}>
-          <TodoItem todo={item} onToggle={this.toggle.bind(this)}/>
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+          onDelete={this.delete.bind(this)}/>
         </li>
       )
     })
@@ -42,7 +43,7 @@ class App extends Component {
       id:idMaker(),
       title:event.target.value,
       status:null,
-      delated:false
+      deleted:false
     })
     this.setState({
       newTodo:'',
@@ -58,6 +59,10 @@ class App extends Component {
       newTodo:event.target.value,
       todoList:this.state.todoList
     })
+  }
+  delete(event,todo){
+    todo.deleted=true;
+    this.setState(this.state)
   }
 }
 
